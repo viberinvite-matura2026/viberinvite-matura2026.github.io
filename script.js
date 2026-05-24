0cabd5963df544addd6ee37c514284e3b5f0721e
viberinvite-matura2026 <markoserafimovski007@gmail.com>
2026-05-24 12:56:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-let vouchers = 5;
+let studyVouchers = 5;
+let relaxVouchers = 5;
 
-function useVoucher(){
+function useStudyVoucher(){
 
-  if(vouchers > 0){
+  if(studyVouchers > 0){
 
-    vouchers--;
+    studyVouchers--;
 
-    document.getElementById("voucher-count").innerText = vouchers;
+    document.getElementById("study-count").innerText =
+    studyVouchers;
+  }
+}
+
+function useRelaxVoucher(){
+
+  if(relaxVouchers > 0){
+
+    relaxVouchers--;
+
+    document.getElementById("relax-count").innerText =
+    relaxVouchers;
   }
 }
